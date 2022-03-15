3cfecd23d2915f1799e143ccee80b5ce886037e6
AnnieMwangi <anne.mwangechi@gmail.com>
2022-03-15 12:23:46 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,17 @@
  * main - prints _putchar followed by a new line
  * Return: 0
  */
-
 int main(void)
 {
-	char c[10] = "_putchar";
-	int i = 0;
-	while (i < 9)
-	{
-		_putchar(c[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
