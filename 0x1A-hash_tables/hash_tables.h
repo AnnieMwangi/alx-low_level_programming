3ccf95439f6888c6a190ae7c159254f0ccfab36b
AnnieMwangi <anne.mwangechi@gmail.com>
2022-07-01 08:38:30 +0300

    hash tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -20,7 +20,7 @@ typedef struct hash_node_s
 } hash_node_t;
 
 /**
- * struct hash_tables_s - Hash table data structure
+ * struct hash_table_s - Hash table data structure
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
